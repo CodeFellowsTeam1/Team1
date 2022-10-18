0365426a3066d3e73dc6da16f075799528c242a4
Caelinh <Caelinhargrave@gmail.com>
2022-10-18 16:40:11 -0500

    Created all fragments for mvp in Navmap

--- app/src/main/java/com/frazmatic/logitrack/MainActivity.java
+++ app/src/main/java/com/frazmatic/logitrack/MainActivity.java
@@ -1,20 +1,14 @@
 package com.frazmatic.logitrack;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
-import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.auth.AuthProvider;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.generated.model.Todo;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Amplify.Auth.signInWithSocialWebUI(AuthProvider.google(), this,
-                result -> Log.i("AuthQuickstart", result.toString()),
-                error -> Log.e("AuthQuickstart", error.toString())
-        );
+
+        Button loginBtn = findViewById(R.id.MainActivityLoginBtn);
+        loginBtn.setOnClickListener( view ->{
+            Amplify.Auth.signInWithSocialWebUI(AuthProvider.google(), this,
+                    result -> Log.i("AuthQuickstart", result.toString()),
+                    error -> Log.e("AuthQuickstart", error.toString())
+            );
+            Intent goToNavHost = new Intent(MainActivity.this, NavHostActivity.class);
+            startActivity(goToNavHost);
+        });
+
+
 
     }
 
---
+++ app/src/main/java/com/frazmatic/logitrack/NavHostActivity.java
@@ -0,0 +1,14 @@
+package com.frazmatic.logitrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavHostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_host);
+    }
+}
