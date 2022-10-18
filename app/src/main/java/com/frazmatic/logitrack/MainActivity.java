db810f39f78110dfa60005fb10e7973c031d12c4
Joshua Frazer <joshua.frazer.work@gmail.com>
2022-10-18 11:33:24 -0700

    location finding refactored

--- app/src/main/java/com/frazmatic/logitrack/MainActivity.java
+++ app/src/main/java/com/frazmatic/logitrack/MainActivity.java
@@ -12,44 +12,50 @@ import android.util.Log;
 
 import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.generated.model.Todo;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.location.Priority;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LocationCallback locationCallback;
+    protected LocationRequest locationRequest;
+    protected LocationCallback locationCallback;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Todo todo = Todo.builder()
-                .name("My first todo")
-                .description("todo description")
-                .build();
-
-        Amplify.API.mutate(
-                ModelMutation.create(todo),
-                response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
-                error -> Log.e("MyAmplifyApp", "Create failed", error)
-        );
         Amplify.Auth.fetchAuthSession(
-                result -> Log.i("AmplifyQuickstart", result.toString()),
+                result -> {
+                    Log.i("AmplifyQuickstart", result.toString());
+                },
                 error -> Log.e("AmplifyQuickstart", error.toString())
         );
         requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 55555);
-        locationRequest = LocationRequest.create();
-        locationRequest.setInterval(100);
-        locationRequest.setFastestInterval(50);
-        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        createLocationRequest(10);
+        createLocationCallback();
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resumeLocationUpdates();
+    }
+
+    private void createLocationRequest(int intervalSeconds){
+        long milliseconds = intervalSeconds * 1000;
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(milliseconds);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+    }
+
+    public void createLocationCallback(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -67,19 +73,15 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
                 for (Location location : locationResult.getLocations()) {
-                    String lat = ((Double) location.getLatitude()).toString();
-                    String lon = ((Double) location.getLongitude()).toString();
-                    Log.i("Lat", lat);
-                    Log.i("Lon", lon);
-
+                    //TODO: Call a method do something with location
+                    String lat = ((Double)location.getLatitude()).toString();
+                    Log.i("Lat: ", lat);
                 }
             }
         };
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public void resumeLocationUpdates(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
